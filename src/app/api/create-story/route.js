e109f9547b8cafb1a26a8d9c6c40dee995ee35a7
podgloy <nawarat.saetang@hotmail.com>
2024-04-16 23:10:35 +0700

    edit

--- src/app/api/create-story/route.js
+++ src/app/api/create-story/route.js
@@ -13,14 +13,43 @@ export function POST (req) {
     const input = {
         top_k: 50,
         top_p: 0.9,
-        prompt: `Write a bedtime story about ${topic} for my ${age} year-old daugther named ${name}"`,
-        temperature: 0.6,
+        prompt: `Write a very short bedtime story about ${topic} for my ${age} year-old daugther named ${name}"`,
+        temperature: 0.2,
         max_new_tokens: 1024,
         prompt_template: "<s>[INST] {prompt} [/INST] ",
         presence_penalty: 0,
         frequency_penalty: 0
       };
       
-      for await (const event of replicate.stream("mistralai/mixtral-8x7b-instruct-v0.1", { input })) {
-        process.stdout.write(event.toString());
-      };
+    let answer = "";
+    for await (const event of replicate.stream("mistralai/mixtral-8x7b-instruct-v0.1", { input })) {
+      answer += event.toString();
+      console.log(answer);
+      process.stdout.write(event.toString());
+  };
+
+      const output = await replicate.run(
+        "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+        {
+          input: {
+            width: 768,
+            height: 768,
+            prompt: answer,
+            scheduler: "K_EULER",
+            num_outputs: 1,
+            guidance_scale: 7.5,
+            num_inference_steps: 50
+          }
+        }
+      );
+      console.log(output);
+    
+    
+      return Response.json({
+        status: "ok",
+        answer,
+        output
+      });
+    
+    
+    }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 "use client";
 import axios from "axios";
+import { useState } from "react";
+
 export default function Home() {
+
+  const[answer, setAnswer]=useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
 async function note (event) {
   event.preventDefault();
@@ -15,16 +20,22 @@ async function note (event) {
       topic
     })
     console.log(response.data);
+    setAnswer(response.data.answer)
+    setIsLoading(false);
   }
 
+
   return (
-    <form onSubmit={note} className="flex flex-col">
+    <div>
+      <form onSubmit={note} className="flex flex-col">
 
-      <input className="border-slate-500" type="text" name="name" placeholder="กรอกชื่อ" />
-      <input className="border-slate-500" type="number" name="age" placeholder="กรอกอายุ" />
-      <input className="border-slate-500" type="text" maxLength="20" name="topic" placeholder="กรอกหัวข้อที่ต้องการ" />
-      <button type="submit">Send</button>
+        <input className="border-slate-500" type="text" name="name" placeholder="กรอกชื่อ" />
+        <input className="border-slate-500" type="number" name="age" placeholder="กรอกอายุ" />
+        <input className="border-slate-500" type="text" maxLength="20" name="topic" placeholder="กรอกหัวข้อที่ต้องการ" />
+        <button type="submit">Send</button>
 
-    </form>
+      </form>
+      <p className="p=4">{answer}</p>
+    </div>
   );
 }
